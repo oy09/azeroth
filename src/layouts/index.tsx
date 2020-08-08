a40f4126cbb8d670715966aee789d915e9563400
ouyangshan09 <ouyangshan09@gmail.com>
2020-08-08 17:33:48 +0800

    impr: 添加组件默认设置

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,7 +4,7 @@ import BasicLayout from './BasicLayout';
 export interface LayoutProps {}
 
 const Layout: React.FC<LayoutProps> = props => {
-  return <BasicLayout>{props.children}</BasicLayout>;
+  return <BasicLayout {...props}>{props.children}</BasicLayout>;
 };
 
 export default Layout;
