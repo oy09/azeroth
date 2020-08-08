8b6f6e45f6c60154ad22b9651b2ee53ddebc2d14
ouyangshan09 <ouyangshan09@gmail.com>
2020-08-08 17:33:18 +0800

    imporve: 添加组件默认设置

---
+++ src/layouts/defaultSettings.ts
@@ -0,0 +1,19 @@
+import { MenuTheme } from 'antd/es/menu/MenuContext';
+
+export interface PureSettings {
+  navTheme: MenuTheme | 'realDark' | undefined;
+  headerHiehgt?: number;
+  fixSiderbar: boolean;
+  title: string;
+  primaryColor?: string;
+}
+
+export type LayoutSetting = PureSettings;
+
+const defaultSettings: LayoutSetting = {
+  navTheme: 'dark',
+  title: '智能管理平台',
+  fixSiderbar: true,
+};
+
+export default defaultSettings;
