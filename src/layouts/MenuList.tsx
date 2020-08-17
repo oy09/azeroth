9683639f6e75f8dcb89da09458823eaf1ba3cc54
ouyangshan09 <ouyangshan09@gmail.com>
2020-08-18 01:08:18 +0800

    impr: 遍历菜单

--- src/layouts/MenuList.tsx
+++ src/layouts/MenuList.tsx
@@ -3,6 +3,7 @@ import { Menu } from 'antd';
 import { MenuProps, MenuMode } from 'antd/es/menu';
 import { MenuTheme } from 'antd/es/menu/MenuContext';
 import { WithFalse, MenuDataItem } from '@/typing';
+import Item from 'antd/lib/list/Item';
 
 export interface MenuListProps {
   className?: string;
@@ -20,6 +21,7 @@ export interface MenuListProps {
 
 const MenuList: React.FC<MenuListProps> = props => {
   const { mode, style, theme, className, menuProps } = props;
+  const utils = new MenuUtil(props);
   console.log('menuData:', props.menuData);
 
   return (
@@ -34,14 +36,66 @@ const MenuList: React.FC<MenuListProps> = props => {
       defaultOpenKeys={[]}
       {...menuProps}
     >
-      <Menu.Item key={1}>菜单1</Menu.Item>
-      <Menu.Item key={2}>菜单2</Menu.Item>
-      <Menu.Item key={3}>菜单3</Menu.Item>
-      <Menu.Item key={4}>菜单4</Menu.Item>
-      <Menu.Item key={5}>菜单5</Menu.Item>
-      <Menu.Item key={6}>菜单6</Menu.Item>
+      {utils.getNavMenu(props.menuData, false)}
     </Menu>
   );
 };
 
 export default MenuList;
+
+class MenuUtil {
+  props: MenuListProps;
+
+  constructor(props: MenuListProps) {
+    this.props = props;
+  }
+
+  getNavMenu(data: MenuDataItem[] = [], isChildren: boolean) {
+    return data
+      .filter(item => item.name && !item.hidden)
+      .map(item => this.getSubNavMenu(item, isChildren))
+      .filter(item => item);
+  }
+
+  getSubNavMenu(data: MenuDataItem, isChildren: boolean) {
+    if (Array.isArray(data.children) && this.hasChildren(data)) {
+      const name = this.getInitName(data);
+      const defaultTitle = (
+        <span>
+          <i className="menu-icon" />
+          <span>{name}</span>
+        </span>
+      );
+
+      const title = defaultTitle;
+
+      return (
+        <Menu.SubMenu title={title} key={data.key || data.path}>
+          {this.getSubNavMenu(data.children, true)}
+        </Menu.SubMenu>
+      );
+    }
+    return (
+      <Menu.Item key={data.key || data.path}>
+        {this.getMenuItemPath(data, isChildren)}
+      </Menu.Item>
+    );
+  }
+
+  getMenuItemPath(data: MenuDataItem, isChildren: boolean) {
+    return data.name;
+  }
+
+  getInitName(data: MenuDataItem) {
+    return data.name;
+  }
+
+  hasChildren(data: MenuDataItem) {
+    return (
+      data &&
+      !data.hidden &&
+      data?.children &&
+      data?.children.some(child => child && !!child.name && !!child.hidden)
+    );
+  }
+}
