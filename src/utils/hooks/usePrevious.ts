1af38f60f6c7205a7075ebe95efba5567f39a473
欧阳 <ouyang@ouyangdeMacBook-Pro.local>
2020-09-25 22:21:36 +0800

    feat: 记录上一个值

---
+++ src/utils/hooks/usePrevious.ts
@@ -0,0 +1,13 @@
+import { useRef, useEffect } from 'react';
+
+const usePrevious = <T, U = T>(state: T): T | undefined => {
+  const ref = useRef<T>();
+
+  useEffect(() => {
+    ref.current = state;
+  });
+
+  return ref.current;
+};
+
+export default usePrevious;
