ccf0ab6a8d57d2a0f58a37b9f5bb3926d57df548
ouyang <ouyangshan09@gmail.com>
2020-10-09 14:09:02 +0800

    format

--- src/components/GlobalHeader/RightContent.tsx
+++ src/components/GlobalHeader/RightContent.tsx
@@ -6,7 +6,7 @@ export interface RightContentProps {}
 const RightContent: React.FC<RightContentProps> = props => {
   return (
     <div className="right-content">
-      <span className="header-action">使用说明1</span>
+      <span className="header-action">使用说明</span>
       <span className="header-action">通知信息</span>
       <span className="header-action">用户菜单</span>
       <span className="header-action">国际化</span>
