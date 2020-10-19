1f066135bd6326061edfc629234d31cd8ee098b1
ouyang <ouyangshan09@gmail.com>
2020-10-19 23:31:23 +0800

    采坑 antd table 组件

--- src/pages/topic/index.tsx
+++ src/pages/topic/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import GridContent from '@/layouts/GridContent';
+import { Table, Card } from 'antd';
+import { ColumnType } from 'antd/lib/table';
 import './topic.scss';
 
 export interface TopicPageProps {
@@ -7,12 +9,133 @@ export interface TopicPageProps {
 }
 
 const TopicPage: React.FC<TopicPageProps> = props => {
+  const columns: ColumnType<any>[] = [
+    {
+      title: '序号',
+      key: 'no',
+      width: 100,
+      align: 'center',
+      render: (text, record, index) => index + 1,
+    },
+    {
+      title: 'id',
+      dataIndex: 'id',
+      key: 'id',
+      align: 'center',
+      width: 140,
+    },
+    {
+      title: '内容',
+      dataIndex: 'content',
+      key: 'content',
+      align: 'center',
+      width: 300,
+    },
+    {
+      title: '图片',
+      dataIndex: 'pictures',
+      key: 'pictures',
+      align: 'center',
+      width: 300,
+    },
+    {
+      title: '联系电话',
+      dataIndex: 'concantMobile',
+      key: 'concantMobile',
+      align: 'center',
+      width: 140,
+    },
+    {
+      title: '联系人',
+      dataIndex: 'concatUser',
+      key: 'concatUser',
+      align: 'center',
+      width: 120,
+    },
+    {
+      title: '发布位置',
+      dataIndex: 'positon',
+      key: 'positon',
+      align: 'center',
+      width: 340,
+    },
+    {
+      title: '置顶状态',
+      dataIndex: 'isTop',
+      key: 'isTop',
+      align: 'center',
+      width: 240,
+    },
+    {
+      title: '发布用户',
+      dataIndex: 'publishUserId',
+      key: 'publishUserId',
+      align: 'center',
+      width: 140,
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'publishTime',
+      key: 'publishTime',
+      align: 'center',
+      width: 200,
+    },
+    {
+      title: '浏览数量',
+      dataIndex: 'readCount',
+      key: 'readCount',
+      align: 'center',
+      width: 120,
+    },
+    {
+      title: '点赞数量',
+      dataIndex: 'likeCount',
+      key: 'likeCount',
+      align: 'center',
+      width: 120,
+    },
+  ];
+  const data = new Array(20)
+    .fill({
+      id: 1,
+      content: '123123123',
+      pictures: '111',
+      concantMobile: '13410825228',
+      concatUser: 'ouyang',
+      positon: '深圳市南山区龙珠大道245号宝珠花园',
+      isTop: 0,
+      publishUserId: '2333',
+      publishTime: '2020-02-01 00:00:00',
+      readCount: 2333,
+      likeCount: 4555,
+    })
+    .map(item => ({ ...item, id: item.id + 1 }));
+
   return (
     <GridContent>
-      <div>tool - 新增/编辑/删除/搜索</div>
-      <div>table - 列、多选、点击编辑、内如预览</div>
+      <Card style={{ padding: 0 }}>
+        <Table
+          columns={columns}
+          dataSource={data}
+          tableLayout="fixed"
+          size="middle"
+          rowSelection={{}}
+          pagination={{
+            showSizeChanger: true,
+            showQuickJumper: true,
+            showTotal: total => `共 ${total}项`,
+            size: 'default',
+          }}
+          scroll={{ x: '100%' }}
+          rowKey="id"
+          sticky
+          bordered
+        />
+      </Card>
+      {/* <div>tool - 新增/编辑/删除/搜索</div> */}
+      {/* <div>table - 列、多选、点击编辑、内如预览</div>
       <div>pagination - 分页</div>
-      <div>add/update form - 添加/编辑表单</div>
+      <div>add/update form - 添加/编辑表单</div> */}
     </GridContent>
   );
 };
