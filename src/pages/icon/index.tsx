50f284acba693609d6148358347c360fc622d17a
ouyang <ouyangshan09@gmail.com>
2020-10-19 11:56:23 +0800

    feat: 图片预览页面

--- src/pages/icon/index.tsx
+++ src/pages/icon/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import GridContent from '@/layouts/GridContent';
-import './icon.scss';
+import * as IconModules from '@ant-design/icons/lib/icons';
+import { Card } from 'antd';
+import styles from './icon.scss';
 
 export interface IconPageProps {
-  //
+  className?: string;
+  style?: CSSProperties;
 }
 
 const IconPage: React.FC<IconPageProps> = props => {
-  return <GridContent>icon-page</GridContent>;
+  const renderAntIcon = () => {
+    return Object.entries(IconModules).map(([key, value]) => {
+      return (
+        <Card.Grid className={styles.iconItem} key={key}>
+          <div className={styles.preview}>{React.createElement(value)}</div>
+          <div className={styles.name}>{key}</div>
+        </Card.Grid>
+      );
+    });
+  };
+
+  return (
+    <GridContent>
+      <Card>{renderAntIcon()}</Card>
+    </GridContent>
+  );
 };
 
 export default IconPage;
