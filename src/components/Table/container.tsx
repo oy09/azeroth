319edb9c15bca005174758d62c5eac8febbc0e79
ouyang <ouyangshan09@gmail.com>
2020-10-27 11:24:43 +0800

    impr: 数据缓存

--- src/components/Table/container.tsx
+++ src/components/Table/container.tsx
@@ -1,15 +1,37 @@
 import { createContainer } from 'unstated-next';
+import { useRef } from 'react';
+import { TableProps } from './Table';
 
 export type ColumnState = {
   //
 };
 
+export type DensityType = 'middle' | 'small' | 'large' | undefined;
+
 export interface UseCounterProps {
-  //
+  columns?: any[];
+  columnStateMap?: { [key: string]: ColumnState };
+  onColumnStateChange?: (map: { [key: string]: ColumnState }) => void;
+  size?: any;
+  onSizeChange?: (size: any) => void;
 }
 
 function useCounter(props: UseCounterProps = {}) {
-  return {};
+  const propsRef = useRef<TableProps<any, any>>();
+
+  return {
+    action: {},
+    setAction: (action: any) => null,
+    sortKeyColumns: [],
+    setSortKeyColumns: () => null,
+    columns: props.columns,
+    setCoumns: () => null,
+    tableSize: 'middle' as DensityType,
+    setTableSize: (size: DensityType) => null,
+    columnsMap: {},
+    setCoumnsMap: () => null,
+    propsRef,
+  };
 }
 
 const Counter = createContainer<ReturnType<typeof useCounter>, UseCounterProps>(
