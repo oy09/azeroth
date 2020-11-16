3a131a98bb288e7fa53f067861ec9b5cccb4549b
ouyang <ouyangshan09@gmail.com>
2020-11-16 15:21:41 +0800

    impr: 为全局注入默认配置

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import '@/styles/index.scss';
 import { createLogger } from 'redux-logger';
+import React from 'react';
+import RequestConfigContext, { Config } from '@/utils/hooks/requestConfigContext';
 
 export const dva = {
   config: {
@@ -8,4 +10,16 @@ export const dva = {
       console.log('dva error:', e);
     },
   },
+};
+
+export const rootContainer = (container: React.ReactNode) => {
+  return React.createElement<React.ProviderProps<Partial<Config>>>(
+    RequestConfigContext.Provider,
+    {
+      value: {
+        baseURL: BASE_URL,
+      },
+    },
+    container,
+  );
 };
