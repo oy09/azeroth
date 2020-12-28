e497d3f04e540ae6555220728cb5c1e613d0dfc5
ouyang <ouyangshan09@gmail.com>
2020-12-28 11:52:41 +0800

    feat: 简单判断是否登录跳转

--- src/layouts/AuthorizeLayout.tsx
+++ src/layouts/AuthorizeLayout.tsx
@@ -1,10 +1,13 @@
 import './AuthorizeLayout.scss';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { PageLoading } from '@ant-design/pro-layout';
+import { useStore, Redirect } from 'umi';
 import BasicLayout from './BasicLayout';
 import { BreadcrumbItemType } from '@/utils/getBreadcrumbProps';
 import RightContent from '@/components/GlobalHeader/RightContent';
 import classnames from 'classnames';
-import { head } from 'lodash';
+import { head, isEmpty } from 'lodash';
+import { stringify } from 'qs';
 
 export interface LayoutProps {
   className?: string;
@@ -12,22 +15,30 @@ export interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = props => {
   const [collapsed, setCollapsed] = useState(() =>
-    localStorage.getItem('sidebarState')
-      ? !!+(localStorage.getItem('sidebarState') as string)
-      : false,
+    localStorage.getItem('sidebarState') ? !!+(localStorage.getItem('sidebarState') as string) : false,
   );
+  const [ready, setReady] = useState<boolean>(false);
 
-  const breadcrumbDataRender = (
-    data: BreadcrumbItemType[],
-  ): BreadcrumbItemType[] => {
+  const { getState, dispatch } = useStore();
+
+  // 判断是否登录
+  useEffect(() => {
+    setReady(true);
+    dispatch({
+      type: 'user/getUser',
+    });
+    dispatch({
+      type: 'user/getMenu',
+    });
+  }, []);
+
+  const breadcrumbDataRender = (data: BreadcrumbItemType[]): BreadcrumbItemType[] => {
     const isHome = (route?: BreadcrumbItemType): boolean => {
       const name = route && route.path;
       if (!name) {
         return false;
       }
-      return (
-        name.trim().toLocaleLowerCase() === '/home'.trim().toLocaleLowerCase()
-      );
+      return name.trim().toLocaleLowerCase() === '/home'.trim().toLocaleLowerCase();
     };
     if (!isHome(head<BreadcrumbItemType>(data))) {
       data.unshift({ path: '/home', name: '首页' });
@@ -47,6 +58,28 @@ const Layout: React.FC<LayoutProps> = props => {
   };
 
   const classNames = classnames('authorize-layout', props.className);
+
+  const {
+    user: { user },
+    loading: {
+      models: { user: loading },
+    },
+  } = getState();
+
+  const isLogin = !isEmpty(user);
+
+  // 页面加载
+  if ((!isLogin && loading) || !ready) {
+    return <PageLoading />;
+  }
+
+  // 判断是否登录
+  if (!isLogin && window.location.pathname !== '/user/login') {
+    const queryString = stringify({
+      redirect: window.location.href,
+    });
+    return <Redirect to={`/user/login?${queryString}`} />;
+  }
 
   return (
     <BasicLayout
