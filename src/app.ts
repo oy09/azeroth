77062be2afa48359d18471ff09e84065972d2017
ouyang <ouyangshan09@gmail.com>
2020-12-28 11:52:04 +0800

    chore: 框架logger过滤

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,20 @@ import { createLogger } from 'redux-logger';
 import React from 'react';
 import RequestConfigContext, { Config } from '@/utils/hooks/requestConfigContext';
 
+const frameworkReg = new RegExp('@@');
+
 export const dva = {
   config: {
-    onAction: createLogger(),
+    onAction: createLogger({
+      collapsed: true,
+      predicate: (state, action) => {
+        const type = action.type as string;
+        if (type.match(frameworkReg)) {
+          return false;
+        }
+        return true;
+      },
+    }),
     onError(e: Error) {
       console.log('dva error:', e);
     },
