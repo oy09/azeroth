2c4f3a0a3a49aaf1139e27610398673c5c1ca53d
ouyang <ouyangshan09@gmail.com>
2021-01-06 11:56:29 +0800

    feat: 用户管理UI

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,13 +1,137 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
 import GridContent from '@/layouts/GridContent';
+import { AzTable } from '@/components/Table';
+import { AzColumnType } from '@/components/Table/Table';
+import FooterToolbar from '@/components/FooterToolbar';
+import Dialog from '@/components/Dialog';
 import './user.scss';
+import { Button } from 'antd';
 
 export interface UserPageProps {
-  //
+  className?: string;
 }
 
 const UserPage: React.FC<UserPageProps> = props => {
-  return <GridContent>menu-page</GridContent>;
+  const formRef = useRef();
+  const actionRef = useRef();
+  const [row, setRow] = useState<any>();
+  const [selectRow, setSelectRow] = useState<any[]>([]);
+  const [createDialogVisible, handleCreateDialogVisible] = useState<boolean>(false);
+  const [updateDialogVisible, handleUpdateDialogVisible] = useState<boolean>(false);
+  const columns: AzColumnType<any>[] = [
+    {
+      title: '序号',
+      hideInSearch: true,
+      width: 50,
+      align: 'center',
+      render: (_1, _2, index) => index + 1,
+    },
+    {
+      title: '用户名',
+      dataIndex: 'account',
+      width: 140,
+      align: 'center',
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      width: 100,
+      align: 'center',
+    },
+    {
+      title: '用户类型',
+      dataIndex: 'createType',
+      width: 120,
+      align: 'center',
+    },
+    {
+      title: '手机号码',
+      dataIndex: 'mobile',
+      width: 140,
+      align: 'center',
+    },
+    {
+      title: '昵称',
+      dataIndex: 'nick',
+      width: 120,
+      align: 'center',
+    },
+    {
+      title: '性别',
+      dataIndex: 'gender',
+      width: 120,
+      align: 'center',
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'createdTime',
+      width: 180,
+      align: 'center',
+    },
+    {
+      title: '操作',
+      width: 80,
+      align: 'center',
+      fixed: 'right',
+      render: (_, row) => {
+        return <a onClick={() => handleUpdateDialog(row)}>编辑</a>;
+      },
+      hideInSearch: true,
+    },
+  ];
+
+  const handleUpdateDialog = (row: any) => {
+    //
+  };
+
+  return (
+    <GridContent>
+      <AzTable
+        columns={columns}
+        formRef={formRef}
+        actionRef={actionRef}
+        toolbarLeftRender={props => (
+          <React.Fragment>
+            <Button type="primary" icon={<PlusOutlined />} onClick={() => handleCreateDialogVisible(true)}>
+              新建
+            </Button>
+          </React.Fragment>
+        )}
+        rowSelection={{
+          onChange: (_, rows) => {
+            setSelectRow(rows);
+          },
+        }}
+        search={{
+          labelWidth: 120,
+        }}
+        scroll={{ x: '100%' }}
+        rowKey="id"
+        bordered
+        sticky
+      />
+      {selectRow.length > 0 && (
+        <FooterToolbar
+          extra={
+            <div>
+              已选择 <a style={{ fontWeight: 600 }}>{selectRow.length}</a> 项 &nbsp;&nbsp;
+            </div>
+          }
+        >
+          <Button danger type="primary">
+            批量删除
+          </Button>
+        </FooterToolbar>
+      )}
+      <Dialog title="添加用户" visible={createDialogVisible} onCancel={() => handleCreateDialogVisible(false)}>
+        1
+      </Dialog>
+      <Dialog title="编辑用户" visible={updateDialogVisible} onCancel={() => handleUpdateDialogVisible(false)}>
+        2
+      </Dialog>
+    </GridContent>
+  );
 };
 
 export default UserPage;
