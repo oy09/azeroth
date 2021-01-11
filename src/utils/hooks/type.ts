13597b25041126b00a3ea0dee88f6b4e9165bd55
ouyang <ouyangshan09@gmail.com>
2021-01-11 11:10:04 +0800

    doc: useReqeust 优化点

--- src/utils/hooks/type.ts
+++ src/utils/hooks/type.ts
@@ -3,9 +3,21 @@ import { AxiosResponse, AxiosRequestConfig } from 'axios';
 export type OnInterceptorsSuccess<T> = (value: T) => T | Promise<T>;
 export type OnInterceptorsFailure<T> = (value: Error) => any;
 
-// 请求选项
+/**
+ * 请求选项
+ * 优化点
+ * 1. 请求拦截改为单项配置，例如以下
+ * requestFulfiled: Function, Function[]
+ * requestRejected: Function, Function[]
+ * responseFulfiled: Function, Function[]
+ * responseFulfiled: Function, Function[]
+ * 2. responseFulfiled项拦截器，做二次封装，方法传入AxiosResponse，但返回只返回数据（responseData），返回的数据由useRequest处理
+ * responseFulfiled: (AxiosResponse) => ResponseData
+ */
 export interface RequestOptions extends Omit<AxiosRequestConfig, 'adapter'> {
-  // 请求拦截器
+  /**
+   * 请求拦截器
+   */
   requestInterceptors?: [
     {
       onFulfiled?: OnInterceptorsSuccess<AxiosRequestConfig>;
