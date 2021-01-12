c8de6003a3501dad9664d88ab3b48eda58e984d7
ouyangshan09 <ouyangshan09@gmail.com>
2021-01-12 22:47:39 +0800

    feat: 字符串切割改进

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import GridContent from '@/layouts/GridContent';
+import { sliceTextToArray, sliceTextIndex } from '@/utils/stringUtils';
 import './Home.scss';
 
 export interface HomePageProps {
@@ -9,5 +10,10 @@ export interface HomePageProps {
 const HomePage: React.FC<HomePageProps> = props => {
   return <GridContent>Home page</GridContent>;
 };
+
+const content =
+  '保险条款中包含免除保险人责任的条款，根据《保险法》第十七条，我现在向您阅读保险合同中免除保险人责任的条款，工银安盛人寿附加养老年金保险责任免除条款,因下列情形之一，导致被保险人身故的，我们不承担给付身故保险金的责任：一、您对被保险人的故意杀害、故意伤害；二、被保险人故意犯罪或者抗拒依法采取的刑事强制措施； 三、被保险人自本附加合同成立或者合同效力恢复之日起2年内自杀，但被保险人自杀时为无民事行为能力人的除外。发生上述第一项情形导致被保险人身故的，本附加合同效力终止，我们将向被保险人的继承人退还本附加合同的现金价值。发生上述其他情形导致被保险人身故的，本附加合同效力终止，我们将向您退还本附加合同的现金价值。请您特别关注：除以上内容外，我司已在保险条款中对于免除我司保险责任的内容进行充分提示，请您务必仔细阅读，确认您已充分知悉责任免除的情形。 ';
+
+console.log('slice array:', sliceTextToArray(content));
 
 export default HomePage;
--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,4 +1,4 @@
-import { omitBy } from 'lodash';
+import { omitBy, maxBy } from 'lodash';
 import { parse as qsParse } from 'qs';
 
 export const stringify = (value: any): string => JSON.stringify(value);
@@ -36,3 +36,54 @@ export const omitUndefined = (value: any) => {
  * 获取当前页面路径的查询参数
  */
 export const getPageQuery = () => qsParse(window.location.href.split('?')[1]);
+
+/**
+ * 字符串切割
+ * 输入1 文本
+ * 输出2 限制长度
+ * 输出 字符串数组
+ * @param {string} content
+ * @param {number} limit
+ * @return {array}
+ */
+export const sliceTextToArray = (content: string, limit: number = 50): string[] => {
+  const result: string[] = [];
+  if (!content) {
+    return result;
+  }
+  const totalSize = content.length;
+  let currentSize = totalSize;
+  while (currentSize > limit) {
+    const value = content.substring(0, limit);
+    const sliceIndex = sliceTextIndex(value);
+    console.log('sliceIndex:', sliceIndex);
+    const sliceText = value.substring(0, sliceIndex);
+    result.push(sliceText);
+
+    content = content.substring(sliceIndex);
+    currentSize = content.length;
+  }
+
+  if (Math.abs(currentSize - limit) > 0) {
+    result.push(content);
+  }
+
+  return result;
+};
+
+/**
+ * 寻找文本中合适的切割点，哪种切割点是一个合适的切割点？
+ * 最好是选择字符串最后一个标点符号作为切割点
+ * @param {string} content 文本
+ */
+export const sliceTextIndex = (content: string): number => {
+  // 使用正则表达式 完成对文本内容的符号匹配, 输出map { sign: index }
+  // 统计map最大index字符
+  const record: { sign: string; index: number }[] = [];
+  content.replace(/(。|\.|，|\;|；|、|》|\]|\?|？|\!|！|\%|\:|：)/g, (match, p1, offset) => {
+    // 偏移量+1 是为了保证切割点在符号后
+    record.push({ sign: match, index: offset + 1 });
+    return '';
+  });
+  return maxBy(record, item => item.index)?.index || content.length;
+};
